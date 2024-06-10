32954c415352a3d95b14d5723a6e95cc1365cacf
eddy-qiu <eqiu22@gmail.com>
2024-06-09 23:58:33 -0700

    finished

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../component/todos.css";
+import {Card,CardContent,Grid,ListItemButton,ListItemText,Checkbox,} from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -11,25 +12,25 @@ import "../component/todos.css";
 const Todos = ({ todos }) => {
     const todoList = todos.length ? (
       todos.map((todo) => {
-        return (
+        return (  
           <Grid key={todo.id}>
             <Card>
               {/* Remember, we set the local state of this todo item when the user submits the form in 
               AddTodo.js. All we need to do is return the todo list item */}
               <CardContent>
-                <span style={{ padding: "50px" }}>add todo content</span>
+                <span style={{ padding: "50px" }}>{CardContent}</span>
               </CardContent>
             </Card>
           </Grid>
         );
       })
     ) : (
-      <p>{/*Enter your message here if todo list is empty*/}</p>
+      <p>You have no todo's left</p>
     );
     // Lastly, return the todoList constant that we created above to show all of the items on the screen.
     return (
       <div className="todoCollection" style={{ padding: "10px" }}>
-        {/*call todo list here*/}
+        {todoList}
       </div>
     );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import pfp from "../assets/pfp.jpeg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div className="split left">
+        <div className="centered">
+        <img 
+        src={pfp}
+        ></img>
+        </div>
+        </div>
+        <div className="split right">
+        <div className="centered">
+        <div className="name_title">Eddy Qiu</div>
+        <div className="brief_description">
+          I am a computer science major at the University of Maryland, College Park. I am passionate about innovation and creation and am excited for the Accelerate program this summer!
+        </div>
+        </div>
+        </div>
+        </div>
       </div>
     )
   }
